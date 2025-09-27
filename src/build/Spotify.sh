b3d30ecdd02c6796ef9bd0cb418e8c45c549b91a
Somil Rana <75232955+somilrana@users.noreply.github.com>
2025-09-27 14:27:12 +0530

    Update Spotify.sh

--- src/build/Spotify.sh
+++ src/build/Spotify.sh
@@ -8,6 +8,5 @@ dl_gh "revanced-patches" "anddea" "latest"
 
 # Patch Spotify
 get_patches_key "Spotjfy-anddea"
-j="i"
-get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+get_apkpure "com.spotify.music" "spotify-arm64-v8a" "spotify-music-and-podcasts-for-android/com.spotify.music"
 patch "spotjfy-arm64-v8a" "anddea"

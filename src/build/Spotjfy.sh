af21ffcdd76f07c78e912c8a6e4a80f13edf7630
Somil Rana <75232955+somilrana@users.noreply.github.com>
2025-11-21 15:06:41 +0530

    Create Spotjfy.sh

---
+++ src/build/Spotjfy.sh
@@ -0,0 +1,6 @@
+revanced_dl
+	# Patch Spotjfy Arm64-v8a
+	j="i"
+	get_patches_key "Spotjfy-revanced"
+	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+	patch "spotjfy-arm64-v8a" "revanced"

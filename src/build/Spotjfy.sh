cc5352dbde2bcc455729263eaa6bdf3b452809b7
Somil Rana <75232955+somilrana@users.noreply.github.com>
2025-11-21 15:08:35 +0530

    Refactor Spotjfy.sh for better readability

--- src/build/Spotjfy.sh
+++ src/build/Spotjfy.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+# Revanced build for Instagram only
+source ./src/build/utils.sh
+
+# Download requirements
+revanced_dl() {
+    dl_gh "revanced-patches revanced-cli" "revanced" "latest"
+}
+
+
 revanced_dl
-	# Patch Spotjfy Arm64-v8a
-	j="i"
-	get_patches_key "Spotjfy-revanced"
-	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
-	patch "spotjfy-arm64-v8a" "revanced"
+# Patch Spotjfy Arm64-v8a
+j="i"
+get_patches_key "Spotjfy-revanced"
+get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+patch "spotjfy-arm64-v8a" "revanced"
